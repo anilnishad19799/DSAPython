3732c17bd20d3659bbec6b68ada641610487a3ec
nishadanil <anilnishad19799@gmail.com>
2024-02-05 22:24:26 +0530

    infix to postfix conversion

---
+++ python/stack/infixtopostfix.py
@@ -0,0 +1,12 @@
+""" Here Stack Implementation"""
+
+""" Making fucntion for converting infix to postfix expression"""
+
+precedenceoperator = {"^":5, "*":4, "/":4, "+":3, "-":3, "(":2}
+
+def infixToPostfixConversion(expression):
+    pass
+
+
+expression = "A*B+(C/D)-E*F+G^H"
+infixToPostfixConversion(expression)
