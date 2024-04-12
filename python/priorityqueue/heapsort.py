55951c9da6a137ae336a40d8297ec51355d918a2
nishadanil <anilnishad19799@gmail.com>
2024-04-12 22:32:39 +0530

    heap sort implementation

---
+++ python/priorityqueue/heapsort.py
@@ -0,0 +1,5 @@
+""" Basic step for implementing heap sort 
+    1. Arrange the given array in heap tree which taken n/2 time complexity because it start from half array size to 0 index
+    2. Then apply deletetion of root node and store that value and then apply ehapify function
+    3. repeat this process until it get empty
+    """
