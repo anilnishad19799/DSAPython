ec134690a10cfcf9c07909a8ebbf803899f6f0ed
nishadanil <anilnishad19799@gmail.com>
2024-04-14 12:43:20 +0530

    two pointer and sliding window approach

---
+++ python/TwopointerandSlidingwindowproblem/findconsecutivekmaxsum.py
@@ -0,0 +1,26 @@
+""" Here in this program i am going to use two pointer approach which is used to solve many problem
+    find k consecutive sum max value from array
+"""
+
+""" Problem statement
+    here given k = 4 find 4 consecutive array value which has maximum sum
+"""
+""" This is simple approach uses iterative war"""
+k = 4
+def findmaxKconsecutive(arr, k):
+    sum = 0
+    maxsum = 0
+    for i in range(0, len(arr) - k + 1):
+        j = i+1
+        k = j+1
+        l = k+1
+        sum = arr[i] + arr[j] + arr[k] + arr[l]
+        if sum > maxsum:
+            maxsum = sum
+
+    return maxsum
+
+
+arr = [2,4,9,8,6,10,12]
+maxsumval = findmaxKconsecutive(arr, k)
+print(maxsumval)
