90c9f3dca012b5f804420e1a8906d2ed1cc8c081
nishadanil <anilnishad19799@gmail.com>
2024-04-18 22:28:11 +0530

    add brute force approach for fruitset problem

---
+++ python/TwopointerandSlidingwindowproblem/fruitbakset.py
@@ -0,0 +1,31 @@
+""" Given Task 
+    arr = [3,3,3,1,2,1,1,2,3,3,4]
+    find maximum consecutive ones haivng k zeros to fiiled at most
+    for example at starting 3 consecutive one there but i can fill 2 consecutive zeros and if get third zeros then it should break and reurn maxlen = 5
+    similarly for all the step is repeated to solve the problem
+"""
+
+""" brute force approach """
+arr = [3,3,3,1,2,1,1,2,3,3,4]
+k = 2
+maxlen = 0
+
+n = len(arr) - 1
+for i in range(n):
+    fruitset = set()
+    for j in range(i, n):
+        fruitset.add(arr[j])
+
+        if len(fruitset) > k:
+            break
+
+        if len(fruitset) <= k:
+            maxlen = max(maxlen, j-i+1)
+
+print(maxlen)
+
+
+
+""" Better approach """
+
+
