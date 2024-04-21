fdabce14a6938de640595b7b9ff72c0e81416e40
nishadanil <anilnishad19799@gmail.com>
2024-04-21 22:34:29 +0530

    add youtube code

--- python/TwopointerandSlidingwindowproblem/longestrepeatingcharacterreplacement.py
+++ python/TwopointerandSlidingwindowproblem/longestrepeatingcharacterreplacement.py
@@ -39,3 +39,44 @@ while(r<n):
     r+=1
 
 print(maxlen)
+
+
+
+""" As per youtube answer given approach"""
+""" Brute force approach """
+
+n = len(s)
+maxf = 0
+for i in range(n):
+    hash = {i:0 for i in range(26)}
+    for j in range(i, n):
+        hash[ord(s[j]) - ord('A')] +=1
+        maxf = max(maxf, hash[ord(s[j])- ord('A')])
+        change = (j - i + 1) - maxf
+
+        if change <=k:
+            maxlen = max(maxlen, j-i+1)
+        else:
+            break
+print(maxlen)
+
+
+""" Optimal solution """
+
+l,r,maxlen,maxf = 0,0,0,0
+hash = {i:0 for i in range(26)}
+n = len(s)
+while r < n:
+    hash[ord(s[j]) - ord("A")] +=1
+    maxf = max(maxf, hash[ord(s[j])- ord('A')])
+
+    if (r-l+1) - maxf > k:
+        hash[ord(s[j])-ord('A')] -=1
+        maxf = 0
+        l+=1
+
+    if (r-l+1) - maxf <= k:
+        maxlen = max(maxlen, r-l+1)
+
+    r+=1
+print(maxlen)
