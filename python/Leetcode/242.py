a7613281f028802e5b88ee013075411e4f670647
nishadanil <anilnishad19799@gmail.com>
2024-04-26 21:46:23 +0530

    solve anagram problem

--- python/Leetcode/242.py
+++ python/Leetcode/242.py
@@ -1,6 +1,9 @@
 from collections import defaultdict
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+            
         hash1 = defaultdict(int)
         hash2 = defaultdict(int)
         for i in s:
@@ -10,16 +13,42 @@ class Solution:
             hash2[j]+=1
 
         if len(hash2) > len(hash1):
-            hash
+            temp1 = hash2
+            temp2 = hash1
+        else:
+            temp1 = hash1
+            temp2 = hash2
         
-        for k in hash1.keys():
-            if k in hash2.keys():
-                if hash1[k] != hash2[k]:
+        for k in temp1.keys():
+            if k in temp2.keys():
+                if temp1[k] != temp2[k]:
                     return False
             else:
                 return False
 
         return True
-            
+    
 
-# SULUTIO ISUE = "AB", "A"
+    """
+    Some better approach
+    
+    count = defaultdict()
+    for i in s:
+        count[x]+=1    
+    
+    for i in t:
+        count[x]--
+    
+    for k,v in count.items:
+        if v!=0:
+            return False
+
+    return True
+        
+            
+    """
+    
+
+soultion =  Solution()
+output = soultion.isAnagram("AB", "AB")
+print(output)
