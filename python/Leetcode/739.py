b3b9baae6e72671db85b7734b0a8fbc98aae9c28
nishadanil <anilnishad19799@gmail.com>
2024-05-18 22:13:21 +0530

    not optimal solution

--- python/Leetcode/739.py
+++ python/Leetcode/739.py
@@ -1,21 +1,28 @@
-class Solution:
-    def dailyTemperatures(self, temperatures):
-        n = len(temperatures)
-        output = []
-        for i in range(len(temperatures)):
-            j = i
-            print(i)
-            print("*"*100)
-            while j+1 < n and (temperatures[j] > temperatures[j+1]) :
-                j+=1
-            print(j,i)
-            output.append(j-i+1)
+""" Not optmial Solutions"""
+
+# class Solution:
+#     def dailyTemperatures(self, temperatures):
+#         n = len(temperatures)
+#         output = []
+#         for i in range(len(temperatures)):
+#             j = i+1
+#             print("*"*100)
+#             while j < n and (temperatures[j] <= temperatures[i]) :
+#                 j+=1
+
+#             if j==n:
+#                 j = i
+#             output.append(j-i)
         
-        return output
+#         return output
 
 
                 
-s = Solution()
-temperatures = [73,74,75,71,69,72,76,73]
-out = s.dailyTemperatures(temperatures)
-print(out)
+# s = Solution()
+# # temperatures = [73,74,75,71,69,72,76,73]
+# # temperatures = [30,40,50,60]
+# # temperatures = [30,60,90]
+# temperatures = [89,62,70,58,47,47,46,76,100,70]
+# out = s.dailyTemperatures(temperatures)
+# print(out)
+
