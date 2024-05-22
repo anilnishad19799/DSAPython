36e820457054762fa043ddd7adcfb33b99c590a6
nishadaniljsr <nishadanil.jsr@gmail.com>
2024-05-22 19:15:46 +0530

    bugs sovled if value not found

--- python/testbinaryseach.py
+++ python/testbinaryseach.py
@@ -2,14 +2,17 @@
 def binarySearchTree(arr, val, l, r):
     mid =  (l + r) // 2
     if arr[mid] == val:
-        return mid
+        return mid+1
+    
+    if l==r:
+        return -1
     elif val < arr[mid]:
         return  binarySearchTree(arr, val, l, mid)
     else:
         return binarySearchTree(arr, val, mid+1, r)
         
 
-arr = [1,2,3,4,5,6,7]
+arr = [1,2,3,5,6,7]
 arr.sort()
 val = 4
 n = len(arr)
