1bbbb64369b032539493995f6ceb7f8ca52a1d76
nishadanil <anilnishad19799@gmail.com>
2024-05-28 21:43:20 +0530

    solve koko eating banana problem

--- python/Leetcode/875.py
+++ python/Leetcode/875.py
@@ -4,6 +4,8 @@
 import math
 from typing import List
 
+
+""" Brute Force Approach """
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         mink = 1000000
@@ -18,14 +20,40 @@ class Solution:
         return mink
 
 
-piles = [3,6,7,11]
-h = 8
+""" Binary Search Approach """
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        mink = 1000000
+        l = 1
+        r = max(piles)
+        while l <=  r:
+            mid = (l+r) // 2
+            totalhours = 0
+            for i in piles:
+                totalhours+=math.ceil(i / mid)
+
+            # if totalhours <= h and mink > mid:
+            #     mink = mid
+
+            if totalhours <= h:
+                r = mid - 1
+                mink = mid;
+            else:
+                l = mid+1
+
+        return mink
+
+# piles = [3,6,7,11]
+# h = 8
 
 # piles = [30,11,23,4,20]
 # h = 5
 
 # piles = [30,11,23,4,20]
 # h = 6
+
+piles =[1000000000]
+h = 2
 
 s = Solution()
 output = s.minEatingSpeed(piles, h)
