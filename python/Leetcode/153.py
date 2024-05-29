454a4b752d27cf3d5ae6d9f8f0e0a8a5360f80a0
nishadaniljsr <nishadanil.jsr@gmail.com>
2024-05-29 18:16:09 +0530

    try to solve rotated sorted array searching

--- python/Leetcode/153.py
+++ python/Leetcode/153.py
@@ -1,31 +1,35 @@
 """ find minimum in sorted array """
 
 from typing import List
+import math
+
 class Solution:
     def findMin(self, nums: List[int]) -> int:
+
         l = 0
         r = len(nums) - 1
-
+    
         while l < r:
             print(l,r)
-            if nums[l] <= nums[r]:
-                r = ((l + r) // 2)
-            else:
-                l = ((l + r) // 2)
+            mid = (l+r) // 2
 
-        if nums[l] < nums[r]:
-            return nums[l]
-        else:
-            return nums[r]            
+            if nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+
+        return nums[l]          
 
 
 s = Solution()
 
-# nums = [3,4,5,1,2]
+nums = [1]
+nums = [3,4,5,1,2]
 # nums = [4,5,6,7,0,1,2]
 # nums = [11,13,15,17]
 # nums = [2,1]
 # nums = [3,1,2]
-nums = [2,3,1]
+# nums = [2,3,1]
+# nums = [5,1,2,3,4]
 out = s.findMin(nums)
 print(out)
