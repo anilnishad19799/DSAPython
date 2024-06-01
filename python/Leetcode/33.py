3e34f0969a1bb74ae8b611049d3f62e6b09cca36
nishadanil <anilnishad19799@gmail.com>
2024-06-01 17:00:10 +0530

    remove print line

--- python/Leetcode/33.py
+++ python/Leetcode/33.py
@@ -8,28 +8,34 @@ class Solution:
         l = 0
         r = len(nums) - 1
 
-        while l<r:
-            mid = (l+r) // 2
-            if nums[mid]==target:
-                return mid+1
-            elif nums[l] <= nums[mid] and target <= nums[mid]:
-                r = mid-1
-            elif nums[mid] <= target and target <= nums[r]:
-                low = mid+1
-            else:
-                r = mid-1
-                
 
+        while l <= r:
+            mid = (l + r) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target and target <= nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] <= target and target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+                
         return -1  
 
 
 s = Solution()
 
-# nums = [4,5,6,7,0,1,2]
-# target = 0
+nums = [4,5,6,7,0,1,2]
+target = 0
 # nums = [4,5,6,7,0,1,2]
 # target = 3
-nums = [2,5,4,0]
-target = 2
+# nums = [2,5,4,0]
+# target = 2
 out = s.search(nums, target)
 print(out)
