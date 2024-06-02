c51a265dd62a88ccf2a638faf3a0a3818ee28929
nishadanil <anilnishad19799@gmail.com>
2024-06-02 13:30:01 +0530

    median in two sorted array

---
+++ python/Leetcode/4.py
@@ -0,0 +1,23 @@
+import numpy as np
+from typing import List
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums1.extend(nums2)
+        nums1.sort()
+        i = len(nums1)
+
+        if i == 1:
+            return nums1[0]
+        elif i == 2:
+            return (nums1[0] + nums1[1]) / 2
+        elif i%2 == 1:
+            return nums1[i//2]
+        else:
+            return (nums1[(i//2)-1] + nums1[i//2]) / 2
+    
+s = Solution()
+nums1 = [1,2]
+nums2 = [3,4]
+out = s.findMedianSortedArrays(nums1, nums2)
+print(out)
