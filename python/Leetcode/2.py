8d5eb276c830877f02ece5fa701da8b7d874f7c3
nishadaniljsr <nishadanil.jsr@gmail.com>
2024-06-10 12:30:10 +0530

    solve add two number using linked list

--- python/Leetcode/2.py
+++ python/Leetcode/2.py
@@ -9,40 +9,80 @@ class ListNode:
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
+        # if not l1 and not l2:
+        #     return l1
+
+        # list1 = []
+        # list2 = []
+
+        # while l1:
+        #     list1.append(l1.val)
+        #     l1 = l1.next
+        
+        # while l2:
+        #     list2.append(l2.val)
+        #     l2 = l2.next
+        
+        # if list1:
+        #     list1 = int((''.join(map(str, list1)))[::-1])
+        # else:
+        #     list1 = 0
+
+        # if list2:
+        #     list2 = int((''.join(map(str, list2)))[::-1])
+        # else:
+        #     list2 = 0
+
+        # output = list1+list2
+
+        # output = str(output)[::-1]
+        # output = [int(s) for s in output]
+
+        # newNode = ListNode(output[0])
+        # firstnode = newNode
+        # for i in range(1, len(output)):
+        #     temp = ListNode(output[i])
+        #     newNode.next = temp
+        #     newNode = temp
+        
+        # return firstnode
+
         if not l1 and not l2:
             return l1
-
-        list1 = []
-        list2 = []
-
-        while l1:
-            list1.append(l1.val)
-            l1 = l1.next
         
-        while l2:
-            list2.append(l2.val)
-            l2 = l2.next
-        
-        if list1:
-            list1 = int((''.join(map(str, list1)))[::-1])
-        else:
-            list1 = 0
+        newNode = ListNode()
+        firstnodeflag = True
 
-        if list2:
-            list2 = int((''.join(map(str, list2)))[::-1])
-        else:
-            list2 = 0
+        carry = 0
 
-        output = list1+list2
+        while l1 or l2:
+            sum = 0
+            if l1:
+                sum+=l1.val
+                l1 = l1.next
+            
+            if l2:
+                sum+=l2.val
+                l2 = l2.next
+            
+            sum = sum+carry
+            carry = sum // 10
+            sum = sum % 10
 
-        output = str(output)[::-1]
-        output = [int(s) for s in output]
-
-        newNode = ListNode(output[0])
-        firstnode = newNode
-        for i in range(1, len(output)):
-            temp = ListNode(output[i])
+            temp = ListNode(sum)
             newNode.next = temp
             newNode = temp
+
+            if firstnodeflag:
+                firstnode = newNode
+                firstnodeflag = False
         
+        if carry:
+            temp = ListNode(carry)
+            newNode.next = temp
+            newNode = temp
+            
         return firstnode
+
+
+
