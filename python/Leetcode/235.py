a1298ae34db5e501cd535da9483bb71b0ad62113
nishadanil <anilnishad19799@gmail.com>
2024-06-15 22:22:58 +0530

    solve lowest common anchestor

--- python/Leetcode/235.py
+++ python/Leetcode/235.py
@@ -9,6 +9,18 @@ class TreeNode:
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
 
+        # if not root:
+        #     return None
+
+        # if p.val < root.val < q.val or q.val < root.val < p.val:
+        #     return root
+
+        # if p.val < root.val and q.val < root.val:
+        #     self.lowestCommonAncestor(root.left, p, q)
+        # if p.val > root.val and q.val > root.val:
+        #     self.lowestCommonAncestor(root.right, p, q)
+
+
         if not root:
             return None
 
