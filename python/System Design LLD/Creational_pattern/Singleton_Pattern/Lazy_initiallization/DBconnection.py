2fc219652e1306f209f1276c35941b106197e70b
nishadaniljsr <nishadanil.jsr@gmail.com>
2024-06-20 16:03:36 +0530

    Lazy initialization

---
+++ python/System Design LLD/Creational_pattern/Singleton_Pattern/Lazy_initiallization/DBconnection.py
@@ -0,0 +1,18 @@
+""" Lazy Initialization overcome eager initialization issue 
+    Issue in eager initialization is that whether you call class fucntion or not its object will created
+    in Lazy initialization if class calls then it created object
+"""
+
+class DBConnection:
+    _instance = None
+
+    def __init__(self):
+        # Initialization code
+        # For demonstration purposes, we initialize a simple attribute
+        self.connection_string = "Your database connection string here"
+
+    @staticmethod
+    def get_instance():
+        if DBConnection._instance is None:
+            DBConnection._instance = DBConnection()
+        return DBConnection._instance
