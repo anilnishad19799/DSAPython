34ec3719cd31b881f4d01df4f2303005a7cff355
nishadaniljsr <nishadanil.jsr@gmail.com>
2024-06-20 16:15:16 +0530

    synchronized pattern

---
+++ python/System Design LLD/Creational_pattern/Singleton_Pattern/Synchronized Pattern/DBConnection.py
@@ -0,0 +1,25 @@
+""" To overcome Lazy issue of threading we use synchronized 
+    we use synchronized lock so that one thread can enter at one time
+    
+    Issue in this
+    so many request come in this it will pass only one thread at a time which make it slow
+"""
+
+import threading
+
+class DBConnection:
+    _instance = None
+    _lock = threading.Lock()
+
+    def __init__(self):
+        # Initialization code
+        # For demonstration purposes, we initialize a simple attribute
+        self.connection_string = "Your database connection string here"
+
+    @staticmethod
+    def get_instance():
+        if DBConnection._instance is None:
+            with DBConnection._lock:
+                if DBConnection._instance is None:
+                    DBConnection._instance = DBConnection()
+        return DBConnection._instance
