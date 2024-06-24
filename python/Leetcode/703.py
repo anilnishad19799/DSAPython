9b24259af8402d7c4d476a5c1f8ab017a898b4e0
nishadaniljsr <nishadanil.jsr@gmail.com>
2024-06-24 19:16:17 +0530

    try other approach

--- python/Leetcode/703.py
+++ python/Leetcode/703.py
@@ -4,13 +4,18 @@ class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
         self.k = k
-        self.nums = nums
+        self.nums = sorted(nums, reverse=True)
         
     def add(self, val: int) -> int:
         self.nums.append(val)
 
-        descendsort = sorted(self.nums, reverse=True)
-        print(descendsort)
+        # descendsort = sorted(self.nums, reverse=True)
+        # print(descendsort)
+        for index, allval in enumerate(self.nums):
+            if allval >= val:
+                self.nums.insert(index, val)
+            
+        descendsort = self.nums[self.k-1]
         return descendsort[self.k-1]
 
 
