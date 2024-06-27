b5c77d41961e5cce5b89f3b1eaa246b7d31876af
nishadaniljsr <nishadanil.jsr@gmail.com>
2024-06-27 18:32:53 +0530

    solve kth element in a stream

--- python/Leetcode/703.py
+++ python/Leetcode/703.py
@@ -1,23 +1,42 @@
 from typing import List
+import heapq
 
 class KthLargest:
 
+    """    
+    First Approach with TLE Error
+    def __init__(self, k: int, nums: List[int]):
+            self.k = k
+            self.nums = sorted(nums, reverse=True)
+            
+        def add(self, val: int) -> int:
+            # descendsort = sorted(self.nums, reverse=True)
+            # print(descendsort)
+            index = 0
+            for allval in self.nums:
+                if val >=allval:
+                    self.nums.insert(index, val)
+                    break
+                index+=1
+
+            descendsort = self.nums[self.k-1]
+            return descendsort
+    """
+
     def __init__(self, k: int, nums: List[int]):
         self.k = k
-        self.nums = sorted(nums, reverse=True)
-        
-    def add(self, val: int) -> int:
-        # descendsort = sorted(self.nums, reverse=True)
-        # print(descendsort)
-        index = 0
-        for allval in self.nums:
-            if val >=allval:
-                self.nums.insert(index, val)
-                break
-            index+=1
+        self.minheap = []
 
-        descendsort = self.nums[self.k-1]
-        return descendsort
+    def add(self, val: int) -> int:
+        # pass
+        if len(self.minheap) < self.k:
+            heapq.heappush(self.minheap, val)
+        elif val > self.minheap[0]:
+            heapq.heapreplace(self.minheap, val)
+        
+        return self.minheap[0]
+
+
 
 
 k = 3
