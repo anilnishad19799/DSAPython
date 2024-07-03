3391b482b147a7a61584964177a0e45ed0eecca4
nishadanil <anilnishad19799@gmail.com>
2024-07-03 22:40:53 +0530

    solve desifn twitter system

--- python/Leetcode/355.py
+++ python/Leetcode/355.py
@@ -8,41 +8,43 @@ class Twitter:
 
     def __init__(self):
         self.tweet = defaultdict(list)
+        self.following = defaultdict(set)
+        self.time = 0
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        # pass      
-        self.tweet[userId].append([[],tweetId])
+        self.tweet[userId].append([self.time, tweetId])
+        self.time+=1
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        # return self.tweet[userId]
-        allvalue = []
-        print(self.tweet[userId])
-        allvalue.append(self.tweet[userId][0][1])
-        followvalue = self.tweet[userId][0][0]
-        if followvalue:
-            for i in followvalue:
-                allvalue.extend(self.tweet[i][0][1])
-        return allvalue
+        totaluser = list(self.following[userId]) + [userId]
+        totalvalue = []
+        for user in totaluser:
+            totalvalue.extend(self.tweet[user])
 
+        sortedtweet = sorted(totalvalue, key=lambda x:x[0], reverse=True)[:10]
+
+        return [tweetid for time, tweetid in sortedtweet]
+       
     def follow(self, followerId: int, followeeId: int) -> None:
-        # pass
-        # self.tweet[followerId].extend(self.tweet[followeeId])
-        self.tweet[followerId] = self.tweet[followerId][0][0].append(followeeId)
+        if followerId != followeeId:
+            self.following[followerId].add(followeeId)
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
-        self.tweet[followerId] = self.tweet[followerId][0][0].remove(followeeId)
+        if followeeId in self.following[followerId]:
+            self.following[followerId].remove(followeeId)
+
+   
 
 twitter = Twitter()
-out = twitter.postTweet(1, 5)
+twitter.postTweet(1, 5)
+out = twitter.getNewsFeed(1)
+print("1")
 print(out)
+twitter.follow(1, 2)
+twitter.postTweet(2, 6)
+out = twitter.getNewsFeed(1)
+print(out)
+twitter.unfollow(1, 2)
 out = twitter.getNewsFeed(1)
 print(out)
 
-out = twitter.follow(1, 2)
-print(out)
-
-out = twitter.postTweet(2, 6)
-print(out)
-
-out = twitter.getNewsFeed(1)
-print(out)
