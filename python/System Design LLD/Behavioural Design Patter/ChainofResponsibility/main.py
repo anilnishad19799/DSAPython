de3a56570463c2569495186e81e46e279cb4db05
nishadaniljsr <nishadanil.jsr@gmail.com>
2024-07-23 18:35:54 +0530

    Behavioural\ Design\ Patter

--- python/System Design LLD/Behavioural Design Patter/ChainofResponsibility/main.py
+++
@@ -1,9 +0,0 @@
-from abstracthandler import Logger
-from setupchain import get_chain_of_loggers
-
-if __name__ == "__main__":
-    logger_chain = get_chain_of_loggers()
-
-    logger_chain.log_message(Logger.INFO, "This is an informational message.")
-    logger_chain.log_message(Logger.DEBUG, "This is a debug message.")
-    logger_chain.log_message(Logger.ERROR, "This is an error message.")
