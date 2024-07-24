da2038751bb5091df0429da3b7879027c27a665c
nishadanil <anilnishad19799@gmail.com>
2024-07-24 23:25:51 +0530

    Topological sort implementations

---
+++ python/Graph/Topologicalsort.py
@@ -0,0 +1,5 @@
+""" here implementation of topological sort in Graph 
+
+    how this topological sort work
+    Linear ordering of vertices such that there is an edges between u and v and u is appear before v in that order
+"""
