b1f22cfefac2fda5ef19efe7ad4768978d7c771e
nishadanil <anilnishad19799@gmail.com>
2024-07-24 23:25:25 +0530

    DFS implementations

---
+++ python/Graph/DFS.py
@@ -0,0 +1,6 @@
+""" Here implementation of DFS in Graph 
+
+    Work same as preorder in BST go in depth and make node visited and similarly visited all node
+    Stack data strucure is used for implementing DFS in Graph
+"""
+
